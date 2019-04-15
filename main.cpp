f675c850eeced2a89a123495c87ccbcff0087724
phabashi <pejmanHabashi@gmail.com>
2019-04-14 22:27:02 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class Problem {
+    public:
+        int Solution(int n) {
+        
+        }
+};
+
+int main( int argc, char* argv[] ) {
+    using namespace std;
+
+    Problem p;
+    p.Solution(2);
+    return 0;
+}
